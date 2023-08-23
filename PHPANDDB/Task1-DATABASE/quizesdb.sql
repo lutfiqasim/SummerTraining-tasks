a099f9f52eac42a50f50222bd5b97642b84beb99
lutfiqasim <lutfiqasim2002@gmail.com>
2023-08-23 13:58:22 +0300

    updated DB

--- PHPANDDB/Task1-DATABASE/quizesdb.sql
+++ PHPANDDB/Task1-DATABASE/quizesdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Aug 16, 2023 at 06:48 PM
+-- Generation Time: Aug 23, 2023 at 12:57 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -38,9 +38,6 @@ CREATE TABLE `answers` (
 --
 
 INSERT INTO `answers` (`id`, `answerSyntax`, `questionId`) VALUES
-(4, 'The World Wide Web Consortium', 2),
-(5, 'Mozilla', 2),
-(6, 'Google', 2),
 (10, '1000', 4),
 (11, '1010', 4),
 (12, '112', 4),
@@ -50,8 +47,6 @@ INSERT INTO `answers` (`id`, `answerSyntax`, `questionId`) VALUES
 (19, '1001', 7),
 (20, '1000', 7),
 (21, '1010', 7),
-(62, '1939 ', 29),
-(63, '1954', 29),
 (65, 'True', 30),
 (66, 'False', 30),
 (67, 'Romaine lettuce', 31),
@@ -60,7 +55,15 @@ INSERT INTO `answers` (`id`, `answerSyntax`, `questionId`) VALUES
 (225, 'Choice1 updated', 93),
 (226, 'Choice 2 updated', 93),
 (227, 'Choice 3 updated', 93),
-(228, 'Added new choice', 93);
+(228, 'Added new choice', 93),
+(237, 'Hyper Text Markup Language', 97),
+(238, 'Hyperlinks and Text Markup Language', 97),
+(244, 'new choice 1', 93),
+(245, 'new choice 2', 93),
+(246, 'new choice 3', 93),
+(281, 'CorrectAnswer', 115),
+(282, 'Choice1', 115),
+(283, 'b', 4);
 
 -- --------------------------------------------------------
 
@@ -71,22 +74,45 @@ INSERT INTO `answers` (`id`, `answerSyntax`, `questionId`) VALUES
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL,
   `question-Syntax` varchar(255) NOT NULL,
-  `correctAnswer` int(11) DEFAULT NULL
+  `correctAnswer` int(11) DEFAULT NULL,
+  `userId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `question-Syntax`, `correctAnswer`) VALUES
-(2, 'Who is making the Web standards?', 4),
-(4, 'What is 100+100 in binary', 10),
-(6, 'what is 1000+1000 in binary', 16),
-(7, '9 in binary is', 19),
-(29, 'When did  world war 2 start', 62),
-(30, '10+10=20', 65),
-(31, 'Which green is usually found in a Caesar salad?', 67),
-(93, 'Question Syntax updated', 224);
+INSERT INTO `questions` (`id`, `question-Syntax`, `correctAnswer`, `userId`) VALUES
+(4, 'What is 100+100 in binary', 10, 1),
+(6, 'what is 1000+1000 in binary', 16, 1),
+(7, '9 in binary is', 19, 1),
+(30, '10+10=20', 65, 1),
+(31, 'Which green is usually found in a Caesar salad?', 67, 1),
+(93, 'Question Syntax updated', 224, 1),
+(97, 'Choose the correct HTML element for the largest heading:', 237, 1),
+(115, 'User question', 281, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
+(1, 'ex', 'ex@gmail.com', 'bf4fc65a70ed8f1794b7e83d3aaf51d7', 1),
+(2, 'user2', 'user2@gmail.com', 'bf4fc65a70ed8f1794b7e83d3aaf51d7', 0);
 
 --
 -- Indexes for dumped tables
@@ -109,6 +135,13 @@ ALTER TABLE `questions`
   ADD KEY `questions_ibfk_1` (`correctAnswer`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -116,13 +149,19 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=229;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=284;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
